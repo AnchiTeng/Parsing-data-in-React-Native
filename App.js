62a0c8bb605eed9875a7e5bcffac608c66519752
AnchiTeng <anchi1009@gmail.com>
2023-05-03 12:55:14 -0400

    fetch menu

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { FlatList, Text, SafeAreaView, View, StyleSheet } from 'react-native';
+import Menu from './menuItems.json';
 
 export default App = () => {
+  const {menu} =Menu;
+  const Item = ({name,price}) => (
+
+    <View style={menuStyles.innerContainer}>
+      <Text style={menuStyles.itemText}>{name}</Text>
+      <Text style={menuStyles.itemText}>${price}</Text>
+    </View>
+  )
+
+
+  const renderItme = ({item}) => (
+    <Item name={item.title} price={item.price} />
+
+  );
+
   return (
     <SafeAreaView style={menuStyles.container}>
       <Text style={menuStyles.headerText}>Little Lemon Menu</Text>
+      <FlatList
+      data={menu}
+      keyExtractor={({id})=>id}
+      renderItem={renderItme}
+      />
+        
+     
     </SafeAreaView>
   );
 };
 
 const menuStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  headerText: {
-    color: '#F4CE14',
-    fontSize: 30,
-    textAlign: 'center',
-  },
-});
+    container: {
+      flex: 1,
+    },
+    innerContainer: {
+      paddingHorizontal: 40,
+      paddingVertical: 20,
+      backgroundColor: '#495E57',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    itemText: {
+      color: '#F4CE14',
+      fontSize: 22,
+    },
+    headerText: {
+      color: '#F4CE14',
+      fontSize: 30,
+      textAlign: 'center',
+    },
+  }); 
